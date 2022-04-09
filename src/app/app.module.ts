b7b26c3bf8d7139dc31e3d173207333fdbb0a78a
Jose Araque <ja.araque14@gmail.com>
2022-04-09 12:12:04 -0500

    the first one commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { BreadCrumbsComponent } from './shared/bread-crumbs/bread-crumbs.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { LoginComponent } from './auth/login/login.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Grafica1Component } from './pages/grafica1/grafica1.component';
+import { PagesComponent } from './pages/pages.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RegisterComponent,
+    NopagefoundComponent,
+    DashboardComponent,
+    BreadCrumbsComponent,
+    SidebarComponent,
+    HeaderComponent,
+    LoginComponent,
+    ProgressComponent,
+    Grafica1Component,
+    PagesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
