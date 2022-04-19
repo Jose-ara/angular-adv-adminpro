890190f8fd65125551a9ef502879b9c0f43cc77f
Jose Araque <ja.araque14@gmail.com>
2022-04-18 23:15:46 -0500

    fin seccion 4 - routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
+import { PagesModule } from './pages/pages.module';
 import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
+import { AuthModule } from './auth/auth.module';
 
 import { AppComponent } from './app.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { BreadCrumbsComponent } from './shared/bread-crumbs/bread-crumbs.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { LoginComponent } from './auth/login/login.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { PagesComponent } from './pages/pages.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    BreadCrumbsComponent,
-    SidebarComponent,
-    HeaderComponent,
-    LoginComponent,
-    ProgressComponent,
-    Grafica1Component,
-    PagesComponent
+    NopagefoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PagesModule,
+    SharedModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
